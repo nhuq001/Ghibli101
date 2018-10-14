dedcc1b9d1d4d02bc5731d757401a4a6a6ee8104
Nhuq001 <nhuq001@fiu.edu>
2018-10-14 01:31:14 -0400

    DOM stuff bby

--- scripts.js
+++ scripts.js
@@ -1,19 +1,31 @@
 var request = new XMLHttpRequest();
-
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
-
 request.onload = function(){
-    var data = JSON.parse(this.response);
 
+    var data = JSON.parse(this.response);
     if(request.status>= 200 && request.status < 400)
     {
         data.forEach(movie => {
-            console.log(movie.title);
+            const card = document.createElement('div');
+            card.setAttribute('class','card')
+            
+            const h1 = document.createElement('h1');
+            h1.textContent = movie.title;
+
+            const p = document.createElement('p');
+            movie.description = movie.description.substring(0,400);
+            p.textContent = `${movie.description}...`;
+
+            container.appendChild(card);
+            card.appendChild(h1);
+            card.appendChild(p);
+
         });
     } else {
-        console.log('error');
+        const errorMessage = document.createElement('marquee');
+        errorMessage.textContent = "Error, it dun goof!";
+        app.appendChild(errorMessage);
     }
 }
-
 
 request.send();
